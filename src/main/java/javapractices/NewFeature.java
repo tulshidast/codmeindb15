b8dd8fdca2d5555940bd05e1eedc637e3c838622
tulshidast <tulsidast77@gmail.com>
2025-05-02 08:26:06 +0530

    changes is new feature

--- src/main/java/javapractices/NewFeature.java
+++ src/main/java/javapractices/NewFeature.java
@@ -3,7 +3,7 @@ package javapractices;
 public class NewFeature {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is from user branch");
 
 	}
 
